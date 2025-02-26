4d0172d97885abf37873c4c10072e2a135a270d9
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-02-26 10:15:52 +0200

    Adding __init__ funtion for self variables

--- car_rental_service.py
+++ car_rental_service.py
@@ -3,5 +3,12 @@ class rentalService:
     model = 0
     year = 0
     rental_price_per_day = 0.0
-    def __init__(self):
+    
+    def __init__(self, brand, model, year, rental_price_per_day):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.rental_price_per_day = rental_price_per_day
+        
+    
          
