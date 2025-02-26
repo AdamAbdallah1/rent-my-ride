9404f2311f3280ca7371c91a2b6446616a271ed8
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-02-26 10:29:06 +0200

    Creating displayInfo method inside the class t display the vehicel rental details and calculating the rental price per day

--- car_rental_service.py
+++ car_rental_service.py
@@ -2,13 +2,20 @@ class rentalService:
     brand = ''
     model = 0
     year = 0
+    seats = 0
     rental_price_per_day = 0.0
     
-    def __init__(self, brand, model, year, rental_price_per_day):
+    def __init__(self, brand, model, year, seats, rental_price_per_day):
         self.brand = brand
         self.model = model
         self.year = year
+        self.seats = seats
         self.rental_price_per_day = rental_price_per_day
         
-    
+    def dislayInfo(self):
+        self.rental_days = rental_days
+        rental_days = float(input("Enter rental days: "))
+        self.rental_price_per_day = rental_price_per_day
+        rental_price_per_day = (rental_days * )
+        
          
