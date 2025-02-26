2144e130ceba0767a080fd31dfda465ecb16a0af
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-02-26 12:04:56 +0200

    Adding show display info function outside class and correcting typos

--- car_rental_service.py
+++ car_rental_service.py
@@ -1,4 +1,4 @@
-class Vehicel:
+class Vehicle:
     brand = ''
     model = 0
     year = 0
@@ -28,7 +28,7 @@ class Vehicel:
     def dislayInfo(self):
         print(f"""  Car: {self.brand}, Year: {self.year}, Seats: {self.seats}, Rental Price: {self.rental_price_per_day}\day""")
 
-class Car(Vehicel):
+class Car(Vehicle):
     def __init__(self, brand, model, year, seats, rental_price_per_day, rental_days):
         super().__init__(brand, model, year, seats, rental_price_per_day, rental_days) #using super() to return a temp object allowing to access the methods
         self.seating_capacity = seats
@@ -36,12 +36,14 @@ class Car(Vehicel):
     def displayInfo(self):
         print(f"""  Car: {self.brand}, Year: {self.year}, Seats: {self.seating_capacity}, Rental Price: ${self.rental_price_per_day}/day""")
 
+class Bike(Vehicle):
+    def __init__(self, brand, model, year, seats, rental_price_per_day, rental_days, engine_capacity):
+        super().__init__(brand, model, year, seats, rental_price_per_day, rental_days)
+        self.engine_capacity = engine_capacity
+        
+    def dislayInfo(self):
+        print(f"Bike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price: ${self.rental_price_per_day}/day")
                 
-toyota = Vehicel("Toyota", "Corolla", 2024, 5, 30, 3)
-toyota.dislayInfo()
+def show_vehicle_info():
+    Vehicle.displayInfo()              
 
-mercedes = Vehicel("Mercedes-Benz", "C-Class", 2024, 5, 50, 3)
-mercedes.dislayInfo()
-
-bmwbike = Vehicel("BMW", "S 100 RR", 2024, 5, 50, 3)
-bmwbike.dislayInfo()
